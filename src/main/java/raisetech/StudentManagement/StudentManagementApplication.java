1f5de9c41082a4bfd71f05052410c8a0ce7f412d
Link L22 <l2202669024as@gmail.com>
2024-12-31 23:01:25 +0900

    スタンダード第５回演習課題コミット

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,26 +1,43 @@
 package raisetech.StudentManagement;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+	private Map<String, String> students = new HashMap<>();
+
+	public StudentManagementApplication() {
+		students.put("Tanaka Ken", "25");
+		students.put("Takahashi Isao", "50");
+		students.put("Satou Iori", "35");
+	}
+
 	public static void main(String[] args) {
 
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello, World";
+	@GetMapping("/students")
+		public Map<String, String>getStudents() {
+		return students;
 	}
 
-	@GetMapping("/greeting")
-	public String greeting() {
-		return "Good morning, everyone!";
+	@PostMapping("/students")
+		public String addstudents(String name, String age) {
+		students.put(name, age);
+		return "Student added successfully";
+
 	}
+
+
 }
+
+
