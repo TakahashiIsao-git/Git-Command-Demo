1033ebbd4299dcbbfd7e7be9bce6f9048f171513
Link L22 <l2202669024as@gmail.com>
2025-01-13 11:30:26 +0900

    Javaエンジニアスタンダード第10～12回時の作成と修正のクラス

--- src/test/java/raisetech/StudentManagement/StudentManagementApplicationTests.java
+++ src/test/java/raisetech/StudentManagement/StudentManagementApplicationTests.java
@@ -1,13 +1,43 @@
 package raisetech.StudentManagement;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootTest
-class StudentManagementApplicationTests {
+@SpringBootApplication
+@RestController
+public class StudentManagementApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  private Map<String, String> students = new HashMap<>();
+
+  public StudentManagementApplicationTests() {
+    students.put("Tanaka Ken", "25");
+    students.put("Takahashi Isao", "50");
+    students.put("Satou Iori", "35");
+  }
+
+  public static void main(String[] args) {
+
+    SpringApplication.run(StudentManagementApplicationTests.class, args);
+  }
+
+  @GetMapping("/students")
+  public Map<String, String>getStudents() {
+    return students;
+  }
+
+  @PostMapping("/students")
+  public String addstudents(String name, String age) {
+    students.put(name, age);
+    return "Student added successfully";
+
+  }
+
+
+}
 
 }
