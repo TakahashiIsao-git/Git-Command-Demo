6f3ec2a73725f1dbd4c29a1a6c5e3313779e223f
Link L22 <l2202669024as@gmail.com>
2025-01-19 11:56:13 +0900

    Revert "Javaエンジニアスタンダード第10～12回時の作成と修正のクラス"
    This reverts commit 25a4be3d0b38c8674d85d845f38fd7c022fcd92f.

--- src/test/java/raisetech/StudentManagement/StudentManagementApplicationTests.java
+++ src/test/java/raisetech/StudentManagement/StudentManagementApplicationTests.java
@@ -1,43 +1,13 @@
 package raisetech.StudentManagement;
 
-import java.util.HashMap;
-import java.util.Map;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
-@RestController
-public class StudentManagementApplicationTests {
+@SpringBootTest
+class StudentManagementApplicationTests {
 
-  private Map<String, String> students = new HashMap<>();
-
-  public StudentManagementApplicationTests() {
-    students.put("Tanaka Ken", "25");
-    students.put("Takahashi Isao", "50");
-    students.put("Satou Iori", "35");
-  }
-
-  public static void main(String[] args) {
-
-    SpringApplication.run(StudentManagementApplicationTests.class, args);
-  }
-
-  @GetMapping("/students")
-  public Map<String, String>getStudents() {
-    return students;
-  }
-
-  @PostMapping("/students")
-  public String addstudents(String name, String age) {
-    students.put(name, age);
-    return "Student added successfully";
-
-  }
-
-
-}
+	@Test
+	void contextLoads() {
+	}
 
 }
