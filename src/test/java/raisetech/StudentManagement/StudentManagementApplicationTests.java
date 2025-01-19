82c664d81fbf5840b8aa21ed9d21fe375c9719c1
Link L22 <l2202669024as@gmail.com>
2025-01-19 12:58:02 +0900

    Javaエンジニアスタンダード第5回演習課題_追加コード1

--- src/test/java/raisetech/StudentManagement/StudentManagementApplicationTests.java
+++ src/test/java/raisetech/StudentManagement/StudentManagementApplicationTests.java
@@ -1,13 +1,23 @@
 package raisetech.StudentManagement;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootTest
-class StudentManagementApplicationTests {
+@RestController
+public class StudentManagementApplicationTests {
 
-	@Test
-	void contextLoads() {
+	private Map<String, String>students = new HashMap<>();
+
+	public StudentManagementApplicationTests() {
+		students.put("TanakaKen", "25");
+		students.put("TakahashiIsao", "50");
+		students.put("SatouIori", "35");
 	}
+
+
 
 }
