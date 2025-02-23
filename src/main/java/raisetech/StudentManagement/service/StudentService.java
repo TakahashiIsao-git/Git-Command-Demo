c71fef83df8a695a04418b58bf2fc79b866a3828
Link L22 <l2202669024as@gmail.com>
2025-02-23 10:09:18 +0900

    Java第11回演習課題のコード処理

--- src/main/java/raisetech/StudentManagement/service/StudentService.java
+++ src/main/java/raisetech/StudentManagement/service/StudentService.java
@@ -1,6 +1,7 @@
 package raisetech.StudentManagement.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import raisetech.StudentManagement.data.Student;
@@ -21,11 +22,15 @@ public class StudentService {
     /* 検索処理
     repository.search();*/
     // 絞り込みをして、年齢が30代の人のみ抽出する。抽出したリストをControllerに返す。
-    return repository.search();
+    return repository.search().stream()
+        .filter(student -> student.getAge() >= 30 && student.getAge() < 40)
+        .collect(Collectors.toList());
   }
 
   public List<StudentsCourses> searchStudentsCoursesList() {
-    // 絞り込み検索で、「Java」コウス情報のみを抽出する。抽出したリストをControllerに返す。
-    return repository.searchStudentsCourses();
+    // 絞り込み検索で、「Java」コース情報のみを抽出する。抽出したリストをControllerに返す。
+    return repository.searchStudentsCourses().stream()
+        .filter(studentsCourses -> "Javaコー".equals(studentsCourses.getCourseName()))
+        .collect(Collectors.toList());
   }
 }
