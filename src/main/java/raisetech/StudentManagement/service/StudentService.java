a2d6c54bbdc21130b91b3ddc04f80a924a577f09
Link L22 <l2202669024as@gmail.com>
2025-02-24 10:02:54 +0900

    Java第20回 Read処理のServiceの実装

--- src/main/java/raisetech/StudentManagement/service/StudentService.java
+++ src/main/java/raisetech/StudentManagement/service/StudentService.java
@@ -22,11 +22,15 @@ public class StudentService {
     /* 検索処理
     repository.search();*/
     // 絞り込みをして、年齢が30代の人のみ抽出する。抽出したリストをControllerに返す。
-    return repository.search();
+    return repository.search().stream()
+        .filter(student -> student.getAge() >= 30 && student.getAge() < 40)
+        .collect(Collectors.toList());
   }
 
   public List<StudentsCourses> searchStudentsCoursesList() {
     // 絞り込み検索で、「Java」コース情報のみを抽出する。抽出したリストをControllerに返す。
-    return repository.searchStudentsCourses();
+    return repository.searchStudentsCourses().stream()
+        .filter(studentsCourses -> "Javaコース".equals(studentsCourses.getCourseName()))
+        .collect(Collectors.toList());
   }
 }
