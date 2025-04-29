c28d41be29787a9b9b3f2e11dae1edbe795f24b6
Link L22 <l2202669024as@gmail.com>
2025-04-29 10:06:38 +0900

    Javaスタンダード第11回演習課題「年齢が30代の人のみを絞り込み検索する」

--- src/main/java/raisetech/StudentManagement/service/StudentService.java
+++ src/main/java/raisetech/StudentManagement/service/StudentService.java
@@ -1,12 +1,11 @@
 package raisetech.StudentManagement.service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
-import raisetech.StudentManagement.repositry.StudentRepository;
+import raisetech.StudentManagement.repository.StudentRepository;
 
 @Service
 public class StudentService {
@@ -18,19 +17,18 @@ public class StudentService {
     this.repository = repository;
   }
 
+  // 全件検索
   public List<Student> searchStudentList() {
-    /* 検索処理
-    repository.search();*/
-    // 絞り込みをして、年齢が30代の人のみ抽出する。抽出したリストをControllerに返す。
+    return repository.search();
+  }
+
+  // 絞り込みをして、年齢が30代の人のみ抽出する。抽出したリストをControllerに返す。
+  public List<Student> search30yearsOldStudentList() {
     return repository.search().stream()
-        .filter(student -> student.getAge() >= 30 && student.getAge() < 40)
-        .collect(Collectors.toList());
+        .filter(student -> student.getAge() >= 30 && student.getAge() <= 39).toList();
   }
 
   public List<StudentsCourses> searchStudentsCoursesList() {
-    // 絞り込み検索で、「Java」コース情報のみを抽出する。抽出したリストをControllerに返す。
-    return repository.searchStudentsCourses().stream()
-        .filter(studentsCourses -> "Javaコー".equals(studentsCourses.getCourseName()))
-        .collect(Collectors.toList());
+    return repository.searchStudentsCourses();
   }
 }
