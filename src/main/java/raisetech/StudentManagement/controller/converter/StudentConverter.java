3f4eeb5b48bf92930cb7901e9bfbd2320709503e
Link L22 <l2202669024as@gmail.com>
2025-06-10 15:40:54 +0900

    Java Standard 21 検索処理部分のリファクタリング（２０２５年６月１０日）

--- src/main/java/raisetech/StudentManagement/controller/converter/StudentConverter.java
+++ src/main/java/raisetech/StudentManagement/controller/converter/StudentConverter.java
@@ -8,11 +8,23 @@ import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
 import raisetech.StudentManagement.domain.StudentDetail;
 
+/**
+ * 受講生詳細を受講生と受講生コース情報、もしくはその逆の変換を行なうコンバーターです。
+ */
 @Component
 public class StudentConverter {
 
+  /**
+   * 受講生に紐づく受講生コース情報をマッピングする。
+   * 受講生コース情報は受講生に対して複数存在するため、ループを回して受講生詳細情報を組み立てる。
+   *
+   * @param students 受講生一覧
+   * @param studentsCourses 受講生コース情報のリスト
+   * @return 受講生詳細情報のリスト
+   */
   public List<StudentDetail> convertStudentDetails(List<Student> students,
       List<StudentsCourses> studentsCourses) {
+    // TODO: コードが複雑なので、シンプルでより良いコードに修正実装する
     List<StudentDetail> studentDetails = new ArrayList<>();
     students.forEach(student -> {
       StudentDetail studentDetail = new StudentDetail();
